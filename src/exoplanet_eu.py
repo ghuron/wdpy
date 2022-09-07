867112173142e318db44cbcee7f4ccfc1b1d15fa
ghuron <andrew.zaikin@gmail.com>
2022-09-07 12:51:46 +0300

    parent (+ creation via SIMBAD), create_snak

--- src/exoplanet_eu.py
+++ src/exoplanet_eu.py
@@ -4,8 +4,10 @@ import re
 import sys
 import time
 import requests
+import os.path
+from simbad_dap import SimbadDAP
 from wikidata import WikiData
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from astropy import coordinates as coord
 
 
@@ -14,6 +16,7 @@ class ExoplanetEu(WikiData):
         super().__init__(login, password)
         self.db_ref = 'Q1385430'
         self.db_property = 'P5653'
+        self.simbad = SimbadDAP(login, password)
         self.constellations = self.query('SELECT DISTINCT ?n ?i {?i wdt:P31/wdt:P279* wd:Q8928; wdt:P1813 ?n}')
 
     def get_summary(self, entity):
@@ -31,6 +34,25 @@ class ExoplanetEu(WikiData):
                 result.append(re.sub('<[^<]+?>', '', record[0]))
         return result
 
+    def obtain_claim(self, entity, snak):
+        if snak is not None and snak['property'] in ['P6257', 'P6258']:
+            if snak['property'] in entity['claims'] or float(snak['datavalue']['value']['amount']).is_integer():
+                return None  # do not update existing coordinates, do not put obviously wrong coordinates
+            self.add_refs(self.obtain_claim(entity, self.create_snak('P6259', 'Q1264450')), [self.db_ref])  # J2000
+        claim = super().obtain_claim(entity, snak)
+        if claim is not None and snak['property'] in ['P4501']:
+            claim['qualifiers'] = {'P4501': [self.create_snak('P1013', 'Q2832068')]}
+        return claim
+
+    def post_process(self, entity):
+        super().post_process(entity)
+        if 'P59' not in entity['claims'] and 'P6257' in entity['claims'] and 'P6258' in entity['claims']:
+            p = coord.SkyCoord(entity['claims']['P6257'][0]['mainsnak']['datavalue']['value']['amount'],
+                               entity['claims']['P6258'][0]['mainsnak']['datavalue']['value']['amount'], frame='icrs',
+                               unit='deg')
+            const = self.constellations[p.get_constellation(short_name=True)]
+            self.obtain_claim(entity, self.create_snak('P59', const if isinstance(const, str) else const[0]))
+
     def parse_sources(self, page):
         patterns = {'(http[s]?://)?(dx\\.)?doi\\.org/': 'haswbstatement:P356=',
                     'http[s]?://(fr\\.)?arxiv\\.org/abs/': 'haswbstatement:P818=',
@@ -44,48 +66,23 @@ class ExoplanetEu(WikiData):
                     'isbn=(\\d{3})(\\d)(\\d{3})(\\d{5})(\\d)': 'haswbstatement:P212=\\g<1>-\\g<2>-\\g<3>-\\g<4>-\\g<5>',
                     '.+jstor\\.org/stable/(info/)?': 'haswbstatement:P356='}
 
-        publications = {}
-        for p in page.find_all('p', {'class': 'publication'}):
-            for a in p.contents:
-                if isinstance(a, Tag) and a.get('href') is not None:
-                    for search_pattern in patterns:
+        result = {}
+        publications = page.find_all('p', {'class': 'publication'})
+        for p in publications:
+            links = p.find_all('a', {'target': '_blank'})
+            for a in links:
+                for search_pattern in patterns:
+                    if a.get('href') is not None:
                         query = re.sub(search_pattern, patterns[search_pattern], a.get('href').strip())
                         if query.startswith('haswbstatement'):
-                            ref_id = self.api_search(query)
-                            if ref_id:
-                                publications[p['id']] = ref_id
+                            if ref_id := self.api_search(query):
+                                result[p.get('id')] = ref_id
                                 break
                             # else:
                             #     print(url + ' is missing')
-        return publications
+        return result
 
-    def build_snak(self, row):
-        digits = 3 + (len(row['value']) - row['value'].find('.') - 1 if row['value'].find('.') > 0 else 0)
-        if row['property'] == 'P6257':
-            ra = row['value'].split(':')
-            row['unit'] = 28390
-            row['value'] = self.format_float(((float(ra[2]) / 60 + float(ra[1])) / 60 + float(ra[0])) * 15, digits)
-        elif row['property'] == 'P6258':
-            dec = row['value'].split(':')
-            row['unit'] = 28390
-            if dec[0].startswith('-'):
-                row['value'] = self.format_float(-((float(dec[2]) / 60 + float(dec[1])) / 60 - float(dec[0])), digits)
-            else:
-                row['value'] = self.format_float(((float(dec[2]) / 60 + float(dec[1])) / 60 + float(dec[0])), digits)
-        return super().build_snak(row)
-
-    def obtain_claim(self, entity, snak):
-        if snak is not None and snak['property'] in ['P6257', 'P6258']:
-            if snak['property'] in entity['claims']:
-                return None  # do not update existing coordinates
-            self.add_refs(self.obtain_claim(entity, self.build_snak({'property': 'P6259', 'value': 'Q1264450'})),
-                          [self.db_ref])  # J2000 epoch
-        claim = super().obtain_claim(entity, snak)
-        if claim is not None and snak['property'] in ['P4501']:
-            claim['qualifiers'] = {'P4501': [self.build_snak({'property': 'P1013', 'value': 'Q2832068'})]}
-        return claim
-
-    def parse_page(self, suffix):
+    def parse_page(self, suffix, force_parent_creation=False):
         response = requests.Session().get("http://exoplanet.eu/catalog/" + suffix)
         if response.status_code != 200:
             return None
@@ -98,80 +95,114 @@ class ExoplanetEu(WikiData):
                       'star_0_stars__ra_0': 'P6257', 'star_0_stars__dec_0': 'P6258'}
         mapping = {'Confirmed': 44559, 'MJ': 651336, 'AU': 1811, 'day': 573, 'deg': 28390, 'JD': 14267, 'TTV': 2945337,
                    'Radial Velocity': 2273386, 'm/s': 182429, 'RJ': 3421309, 'Imaging': 15279026, 'Candidate': 18611609,
-                   'Primary Transit': 2069919, 'Microlensing': 1028022, 'Astrometry': 181505, 'Controversial': 18611609}
+                   'Primary Transit': 2069919, 'Microlensing': 1028022, 'Astrometry': 181505, 'Controversial': 18611609,
+                   'Retracted': 7936582}
         result = []
-        row = {'source': []}
+        current_snak = None
         for td in page.find_all('td'):
             if td.get('id') in properties and td.text != '—':
-                if 'value' in row:
-                    result.append(row)
-                    row = {'source': []}
-                row['property'] = properties[td.get('id')]
+                if current_snak is not None:
+                    result.append(current_snak)
+                    current_snak = None
+
                 if amount := re.search(
                         '(?P<value>\\d[-.e\\d]+)\\s*\\(\\s*(?P<min>-\\S+)\\s+(?P<max>\\+\\d[-.e\\d]+)\\s*\\)(?P<unit>\\s+[A-Za-z]\\S+)?',
                         td.text):
-                    row['value'] = amount.group('value')
-                    row['min'] = amount.group('min')
-                    row['max'] = amount.group('max')
-                    if amount.group('unit'):
-                        row['unit'] = mapping[amount.group('unit').strip()]
+                    current_snak = self.create_snak(properties[td.get('id')], amount.group('value'),
+                                                    amount.group('min'), amount.group('max'))
+                    if current_snak is not None and amount.group('unit'):
+                        current_snak['datavalue']['value']['unit'] = \
+                            'http://www.wikidata.org/entity/Q' + str(mapping[amount.group('unit').strip()])
                 elif amount := re.search(
                         '^(?P<value>\\d[-.e\\d]+)\\s*(\\(\\s*±\\s*(?P<bound>\\d[-.e\\d]+)\\s*\\))?(?P<unit>\\s+[A-Za-z]\\S+)?$',
                         td.text):
-                    row['value'] = amount.group('value')
                     if amount.group('bound'):
-                        row['min'] = '-' + amount.group('bound')
-                        row['max'] = amount.group('bound')
-                    if amount.group('unit'):
-                        row['unit'] = mapping[amount.group('unit').strip()]
+                        current_snak = self.create_snak(properties[td.get('id')], amount.group('value'),
+                                                        '-' + amount.group('bound'), amount.group('bound'))
+                    else:
+                        current_snak = self.create_snak(properties[td.get('id')], amount.group('value'))
+                    if current_snak is not None and amount.group('unit'):
+                        current_snak['datavalue']['value']['unit'] = \
+                            'http://www.wikidata.org/entity/Q' + str(mapping[amount.group('unit').strip()])
+                elif td.text in mapping:
+                    current_snak = self.create_snak(properties[td.get('id')], 'Q' + str(mapping[td.text]))
+                elif properties[td.get('id')] == 'P6257':
+                    digits = 3 + (len(td.text) - td.text.find('.') - 1 if td.text.find('.') > 0 else 0)
+                    ra = td.text.split(':')
+                    current_snak = self.create_snak(properties[td.get('id')], self.format_float(
+                        ((float(ra[2]) / 60 + float(ra[1])) / 60 + float(ra[0])) * 15, digits))
+                    if current_snak is not None:
+                        current_snak['datavalue']['value']['unit'] = 'http://www.wikidata.org/entity/Q28390'
+                elif properties[td.get('id')] == 'P6258':
+                    digits = 3 + (len(td.text) - td.text.find('.') - 1 if td.text.find('.') > 0 else 0)
+                    dec = td.text.split(':')
+                    if dec[0].startswith('-'):
+                        current_snak = self.create_snak(properties[td.get('id')], self.format_float(
+                            -((float(dec[2]) / 60 + float(dec[1])) / 60 - float(dec[0])), digits))
+                    else:
+                        current_snak = self.create_snak(properties[td.get('id')], self.format_float(
+                            ((float(dec[2]) / 60 + float(dec[1])) / 60 + float(dec[0])), digits))
+                    if current_snak is not None:
+                        current_snak['datavalue']['value']['unit'] = 'http://www.wikidata.org/entity/Q28390'
                 else:
-                    row['value'] = 'Q' + str(mapping[td.text]) if td.text in mapping else td.text
-            elif 'value' in row:
+                    current_snak = self.create_snak(properties[td.get('id')], td.text)
+            elif current_snak is not None:
                 if 'showArticle' in str(td):
                     ref_id = re.sub('.+\'(\\d+)\'.+', '\\g<1>', str(td))
                     if ref_id in sources:
-                        row['source'].append(sources[ref_id])
+                        if 'source' not in current_snak:
+                            current_snak['source'] = []
+                        current_snak['source'].append(sources[ref_id])
                 elif 'showAllPubs' not in str(td):
-                    result.append(row)
-                    row = {'source': []}
+                    result.append(current_snak)
+                    current_snak = None
+            elif len(td.attrs) == 0 and td.parent.parent.get('id') == 'table_' + td.text:
+                if (parent_id := self.api_search('haswbstatement:"P3083=' + td.text + '"')) is None:
+                    ident = self.simbad.tap_query('https://simbad.u-strasbg.fr/simbad/sim-tap',
+                                                  'SELECT id, main_id FROM ident JOIN basic ON oid = oidref ' +
+                                                  'WHERE id=\'' + td.text + '\'')
+                    if len(ident) != 1 or not force_parent_creation:
+                        continue
+                    parent = {}
+                    if parent_data := self.simbad.transform_to_snak(list(ident.values())[0]['main_id']):
+                        self.simbad.sync(parent, parent_data, list(ident.values())[0]['main_id'])
+                        parent_id = self.simbad.save(parent)
+                    else:
+                        continue
 
-        if 'value' in row:
-            result.append(row)
+                current_snak = self.create_snak('P397', parent_id)
+
+        if current_snak is not None:
+            result.append(current_snak)
         return result
 
-    def add_constellation(self, item):
-        if 'P59' not in item['claims'] and 'P6257' in item['claims'] and 'P6258' in item['claims']:
-            point = coord.SkyCoord(
-                item['claims']['P6257'][0]['mainsnak']['datavalue']['value']['amount'],
-                item['claims']['P6258'][0]['mainsnak']['datavalue']['value']['amount'],
-                frame='icrs', unit='deg')
-            const = self.constellations[point.get_constellation(short_name=True)]
-            if isinstance(const, str):
-                wd.obtain_claim(item, wd.build_snak({'property': 'P59', 'value': const}))
-            else:
-                wd.obtain_claim(item, wd.build_snak({'property': 'P59', 'value': const[0]}))
 
+if sys.argv[0].endswith(os.path.basename(__file__)):  # if not imported
+    wd = ExoplanetEu(sys.argv[1], sys.argv[2])
+    wd_items = wd.get_all_items('SELECT ?id ?item { ?item p:P5653/ps:P5653 ?id FILTER NOT EXISTS {?item p:P397 []} }')
 
-wd = ExoplanetEu(sys.argv[1], sys.argv[2])
-wd_items = wd.get_all_items('SELECT ?id ?item { ?item wdt:P5653 ?id }')
+    for ex_id in wd_items:
+        # exoplanet_id = 'GJ 2056 b'
+        item = {}
+        if wd_items[ex_id] is not None:
+            try:
+                info = json.loads(wd.api_call('wbgetentities', {'props': 'claims|info|labels', 'ids': wd_items[ex_id]}))
+            except json.decoder.JSONDecodeError:
+                print('Cannot decode wbgetentities response')
+                continue
+            except requests.exceptions.ConnectionError:
+                print('Connection error while calling wbgetentities')
+                continue
+            if 'entities' not in info:
+                continue
+            item = info['entities'][wd_items[ex_id]]
+        # else:
+        #     continue  # uncomment if we do not want to create new items
 
-for exoplanet_id in wd_items:
-    # exoplanet_id = 'Gaia-ASOI-053 b'
-    print(exoplanet_id)
-    if wd_items[exoplanet_id] is not None:
-        info = json.loads(wd.api_call('wbgetentities', {'props': 'claims|info', 'ids': wd_items[exoplanet_id]}))
-        if 'entities' not in info:
-            continue
-        item = info['entities'][wd_items[exoplanet_id]]
-    else:
-        item = {'claims': {}, 'labels': {'en': {'value': exoplanet_id, 'language': 'en'}}}
-        wd.obtain_claim(item, wd.build_snak({'property': 'P5653', 'value': exoplanet_id}))
+        if data := wd.parse_page(ex_id, 'claims' in item and 'P397' not in item['claims']):
+            wd.sync(item, data, ex_id)
+            wd.save(item)
+        else:
+            wd.trace(item, 'was not updated because corresponding exoplanet.eu page was not parsed: ' + ex_id)
 
-    if data := wd.parse_page(exoplanet_id):
-        wd.update(item, data)
-        wd.add_constellation(item)
-        wd.save(item)
-    else:
-        wd.trace(item, 'was not updated because corresponding exoplanet.eu page was not parsed')
-
-    time.sleep(3)
+        time.sleep(2)
