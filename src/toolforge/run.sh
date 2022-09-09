393c23f0950157746c399e9633388e0866e8d81e
tools.wdml <tools.wdml@tools-sgebastion-10.tools.eqiad1.wikimedia.cloud>
2022-09-09 16:00:42 +0000

    update file permission

--- src/toolforge/run.sh
+++ src/toolforge/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-./pyvenv/bin/python ./src/$1 $(< ./src/toolforge/.credentials )
+echo "Starting a new job at" $(date -u) >> ${1//_/-}.out
+#rm -f ${1//_/-}.*
+./pyvenv/bin/python ./src/$1.py $(< ./src/toolforge/.credentials )
