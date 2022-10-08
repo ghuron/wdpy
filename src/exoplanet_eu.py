5d43071114a870e26161cf56ed151a6938e2d0a7
ghuron <andrew.zaikin@gmail.com>
2022-10-08 13:56:54 +0200

    More clarity

--- src/exoplanet_eu.py
+++ src/exoplanet_eu.py
@@ -180,14 +180,14 @@ class ExoplanetEu(WikiData):
                 elif 'showAllPubs' not in str(td) and current_snak is not None:
                     self.input_snaks.append(current_snak)
                     current_snak = None
-            elif len(td.attrs) == 0 and td.parent.parent.get('id') == 'table_' + td.text and parsing_planet:
+            elif len(td.attrs) == 0 and parsing_planet and (td.parent.parent.get('id') == 'table_' + td.text):
                 ident = SimbadDAP.tap_query('https://simbad.u-strasbg.fr/simbad/sim-tap',
                                             'SELECT main_id FROM ident JOIN basic ON oid = oidref ' +
                                             'WHERE id=\'' + td.text + '\'')
                 if len(ident) == 1:
                     no_parent = self.entity and 'claims' in self.entity and 'P397' not in self.entity['claims']
                     if host_id := SimbadDAP.get_by_id(list(ident.keys())[0], no_parent):
-                        current_snak = self.create_snak('P397', host_id)
+                        current_snak = ExoplanetEu.create_snak('P397', host_id)
 
         if current_snak is not None:
             self.input_snaks.append(current_snak)
