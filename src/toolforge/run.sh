d9bc8f8bbbe8c8a6a0f5656478d8baff326764f8
ghuron <andrew.zaikin@gmail.com>
2023-04-07 18:43:39 +0200

    Unified log archiving

--- src/toolforge/run.sh
+++ src/toolforge/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Starting a new job at" $(date -u) >> ${1//_/-}.out
-#rm -f ${1//_/-}.*
-./pyvenv/bin/python ./src/$1.py $(< ./src/toolforge/.credentials )
+./pyvenv/bin/python ./src/${1//-/_}.py $(< ./src/toolforge/.credentials )
+mkdir --parent ./log
+mv ./$1.out log/$1.out.$(date "+%Y%m%d")
+mv ./$1.err log/$1.err.$(date "+%Y%m%d")
