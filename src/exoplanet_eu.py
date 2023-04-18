37d254477804fc400e39bc0993d267ad98806e5c
ghuron <andrew.zaikin@gmail.com>
2023-04-18 09:43:40 +0200

    Remove erroneously committed debug line

--- src/exoplanet_eu.py
+++ src/exoplanet_eu.py
@@ -152,7 +152,7 @@ class ExoplanetEu(ADQL):
 if argv[0].endswith(basename(__file__)):  # if just imported - do nothing
     WikiData.logon(argv[1], argv[2])
     for ex_id, wd_item in WikiData.get_all_items('SELECT ?id ?item {?item p:P5653/ps:P5653 ?id}').items():
-        ex_id, wd_item = 'K03456.02', 'Q21067504'  # uncomment to debug specific item only
+        # ex_id, wd_item = 'K03456.02', 'Q21067504'  # uncomment to debug specific item only
         item = ExoplanetEu(ex_id, wd_item)
         if data := item.retrieve():
             item.prepare_data(data)
