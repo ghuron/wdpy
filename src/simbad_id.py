fe351657884834955f5008805518b7c7ec220e54
ghuron <andrew.zaikin@gmail.com>
2023-11-01 20:26:04 +0100

    Rewrite to update only the safest possible way

--- src/simbad_id.py
+++ src/simbad_id.py
@@ -3,20 +3,18 @@ from sys import argv
 
 from adql import ADQL
 
+TAP = 'https://simbad.u-strasbg.fr/simbad/sim-tap'
+QUERY = 'SELECT id, main_id from ident JOIN basic ON oidref = oid WHERE main_id != id AND id IN (\'{}\')'
+
 ADQL.logon(argv[1], argv[2])
-QUERY = 'SELECT id, main_id from ident JOIN basic ON oidref = oid WHERE id IN ({})'
-offset, size = 0, 10000
-while True:
-    if len(chunk := ADQL.query('SELECT ?i ?s {{?s ps:P3083 ?i}} LIMIT {} OFFSET {}'.format(size, offset))) == 0:
-        quit()
-    ident = ADQL.tap_query('https://simbad.u-strasbg.fr/simbad/sim-tap',
-                           QUERY.format('\'{}\''.format('\',\''.join([i.replace('\'', '\'\'') for i in chunk.keys()]))))
-    for qid, statement in chunk.items():
-        statement = statement.replace('-', '$', 1).replace('statement/', '')
-        if qid not in ident:
-            ADQL(qid).edit({'claim': statement, 'summary': 'no longer in [[Q654724]]'}, 'wbremoveclaims')
-        elif qid != ident[qid][0]['main_id']:
-            item = ADQL(ident[qid][0]['main_id'])
-            (p3083 := item.obtain_claim(ADQL.create_snak('P3083', ident[qid][0]['main_id'])))['id'] = statement
-            item.edit({'claim': dumps(p3083), 'summary': 'was ' + qid}, 'wbsetclaim')
-    offset += size
+offset = -(size := 10000)
+while (offset := offset + size) >= 0:
+    if chunk := ADQL.query('SELECT ?i ?s {{?s ps:P3083 ?i}} LIMIT {} OFFSET {}'.format(size, offset)):
+        if ident := ADQL.tap_query(TAP, QUERY.format('\',\''.join([i.replace('\'', '\'\'') for i in chunk.keys()]))):
+            for simbad_id in ident:
+                if (new_id := ident[simbad_id][0]['main_id']) != simbad_id:
+                    p3083 = (item := ADQL(new_id)).obtain_claim(ADQL.create_snak('P3083', new_id))
+                    p3083['id'] = chunk[simbad_id].replace('-', '$', 1).replace('statement/', '')
+                    item.edit(data={'summary': 'was ' + simbad_id, 'claim': dumps(p3083)}, method='wbsetclaim')
+    elif len(chunk) == 0:
+        break
