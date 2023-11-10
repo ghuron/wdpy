47498bd7ebdfa8db9b4244d406afc4b6cbb801d5
ghuron <andrew.zaikin@gmail.com>
2023-11-10 06:04:44 +0100

    Return lookup for DOI

--- src/arxiv.py
+++ src/arxiv.py
@@ -45,24 +45,31 @@ class ArXiv(Element):
 
     def prepare_data(self, source=None):
         if tree := ArXiv.arxiv_xml('api/query?id_list=' + self.external_id):
-            input_snaks = super().prepare_data()
+            snaks = super().prepare_data() + [ArXiv.create_snak('P31', 'Q13442814')]
             ns = {'w3': 'http://www.w3.org/2005/Atom', 'arxiv': 'http://arxiv.org/schemas/atom'}
             title = ' '.join(tree.findall('*/w3:title', ns)[0].text.split())
-            input_snaks.append(ArXiv.create_snak('P1476', {'text': title, 'language': 'en'}))
+            snaks.append(ArXiv.create_snak('P1476', {'text': title, 'language': 'en'}))
             author_num = 0
             for author in tree.findall('*/*/w3:name', ns):
                 if len(author.text.strip()) > 3:
                     snak = ArXiv.create_snak('P2093', author.text.strip())
                     snak['qualifiers'] = {'P1545': str(author_num := author_num + 1)}
-                    input_snaks.append(snak)
+                    snaks.append(snak)
+            doi = ''
             if len(doi_list := tree.findall('*/arxiv:doi', ns)) == 1:
-                input_snaks.append(self.create_snak('P356', doi_list[0].text.upper()))
-            return input_snaks
+                snaks.append(self.create_snak('P356', doi := doi_list[0].text.upper()))
+            return {'input': snaks, 'label': title, 'doi': doi}
+
+    def update(self, parsed_data):
+        if parsed_data:
+            if self.qid is None and parsed_data['doi']:
+                self.qid = ArXiv.haswbstatement(parsed_data['doi'], 'P356')
+            if 'en' not in self.entity['labels']:
+                self.entity['labels']['en'] = {'value': parsed_data['label'], 'language': 'en'}
+            return super().update(parsed_data['input'])
 
     def obtain_claim(self, snak):
         if snak is not None:
-            if snak['property'] == 'P1476' and 'P1476' in self.entity['claims']:
-                return
             if snak['property'] == 'P2093' and 'qualifiers' in snak and 'P1545' in snak['qualifiers']:
                 for property_id in ['P50', 'P2093']:
                     if property_id in self.entity['claims']:
@@ -70,15 +77,9 @@ class ArXiv(Element):
                             if 'qualifiers' in claim and 'P1545' in claim['qualifiers']:
                                 if claim['qualifiers']['P1545'][0]['datavalue']['value'] == snak['qualifiers']['P1545']:
                                     return
-        return super().obtain_claim(snak)
-
-    def post_process(self):
-        if 'en' not in self.entity['labels'] and 'P1476' in self.entity['claims']:
-            self.entity['labels']['en'] = {
-                'value': self.entity['claims']['P1476'][0]['mainsnak']['datavalue']['value']['text'],
-                'language': 'en'}
-        if 'P31' not in self.entity['claims']:
-            self.obtain_claim(self.create_snak('P31', 'Q13442814'))
+            elif snak['property'] in self.entity['claims']:
+                return
+            return super().obtain_claim(snak)
 
 
 if ArXiv.initialize(__file__):  # if not imported
@@ -89,9 +90,9 @@ if ArXiv.initialize(__file__):  # if not imported
     while offset is not None:
         _, offset = ArXiv.get_next_chunk(offset)
         no_arxiv_items = Wikidata.query(QUERY.format('\' \''.join(ArXiv.dataset.values())))
-        for arxiv_id in ArXiv.dataset:
-            if doi := ArXiv.dataset[arxiv_id]:
-                if no_doi_items and (arxiv_id in no_doi_items):  # DOI is absent
-                    ArXiv.set_id(no_doi_items.pop(arxiv_id), 'P356', doi.upper(), SUMMARY.format('P818', arxiv_id))
-                elif no_arxiv_items and (doi in no_arxiv_items):  # Arxiv is absent
-                    ArXiv.set_id(no_arxiv_items.pop(doi), 'P818', arxiv_id, SUMMARY.format('P356', doi))
+        for p818 in ArXiv.dataset:
+            if p356 := ArXiv.dataset[p818]:
+                if no_doi_items and (p818 in no_doi_items):  # DOI is absent
+                    ArXiv.set_id(no_doi_items.pop(p818), 'P356', p356.upper(), SUMMARY.format('P818', p818))
+                elif no_arxiv_items and (p356 in no_arxiv_items):  # Arxiv is absent
+                    ArXiv.set_id(no_arxiv_items.pop(p356), 'P818', p818, SUMMARY.format('P356', p356))
